fb8e0df4dc4c083d8a12f79b8fda0dc5645bf99c
azui <enzogrutila@gmail.com>
2025-10-15 11:26:23 -0300

    feat:Fiz a tela de cadastro e login, junto com suas logicas

--- Projeto_Pirotech/gestao_pirotech_app/models.py
+++ Projeto_Pirotech/gestao_pirotech_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuarios(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=255)
+    email = models.EmailField()
--- Projeto_Pirotech/gestao_pirotech_app/views.py
+++ Projeto_Pirotech/gestao_pirotech_app/views.py
@@ -1,14 +1,40 @@
-from django.shortcuts import render
-from .forms import CadastroForm, LoginForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    form = CadastroForm()
-    contexto = {'form': form}
+    if request.method == 'GET':
+        return render(request, "gestao_pirotech_app/cadastro.html")
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
 
+        if senha != confirmar_senha:
+            messages.error(request, 'As senhas não coincidem!')
+            return redirect('cadastro')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Este nome de usuário já está em uso!')
+            return redirect('cadastro')
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        
+        return redirect('login')
+    
+def login(request):
     if request.method == 'POST':
-        form = CadastroForm(request.POST)
-        if form.is_valid():
-            form.save()
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
 
-    return render(request, 'gestao_pirotech_app/cadastro.html', contexto)
+        user = authenticate(username=username, password=senha)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, f"Usuário ou senha inválidos, tente novamente!")
+            return redirect("login")
+    else:
+        return render(request, "gestao_pirotech_app/login.html")
