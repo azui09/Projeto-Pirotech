04f23f61d071f4f159d54402042c7f6046a0cd6b
azui <enzogrutila@gmail.com>
2025-10-24 14:28:57 -0300

    feat: terminei o projeto

--- Projeto_Pirotech/gestao_pirotech_app/models.py
+++ Projeto_Pirotech/gestao_pirotech_app/models.py
@@ -1,6 +1,59 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+catalogo_racoes = {
+    'cavalo': {
+        'premium': {
+            '5kg': {'preco_venda': 80.00, 'preco_custo': 72.00},
+            '20kg': {'preco_venda': 280.00, 'preco_custo': 252.00},
+        },
+        'normal': {
+            '5kg': {'preco_venda': 60.00, 'preco_custo': 54.00},
+            '20kg': {'preco_venda': 200.00, 'preco_custo': 180.00},
+        },
+    },
+    'bovino': {
+        'premium': {
+            '10kg': {'preco_venda': 120.00, 'preco_custo': 108.00},
+            '25kg': {'preco_venda': 280.00, 'preco_custo': 252.00},
+        },
+        'normal': {
+            '10kg': {'preco_venda': 90.00, 'preco_custo': 81.00},
+            '25kg': {'preco_venda': 200.00, 'preco_custo': 180.00},
+        },
+    },
+    'ovelha': {
+        'premium': {
+            '2kg': {'preco_venda': 25.00, 'preco_custo': 22.50},
+            '5kg': {'preco_venda': 55.00, 'preco_custo': 49.50},
+        },
+        'normal': {
+            '2kg': {'preco_venda': 18.00, 'preco_custo': 16.20},
+            '5kg': {'preco_venda': 40.00, 'preco_custo': 36.00},
+        },
+    },
+    'porco': {
+        'premium': {
+            '1kg': {'preco_venda': 12.00, 'preco_custo': 10.80},
+            '5kg': {'preco_venda': 55.00, 'preco_custo': 49.50},
+        },
+        'normal': {
+            '1kg': {'preco_venda': 8.00, 'preco_custo': 7.20},
+            '5kg': {'preco_venda': 40.00, 'preco_custo': 36.00},
+        },
+    },
+    'galinha': {
+        'premium': {
+            '0.5kg': {'preco_venda': 10.00, 'preco_custo': 9.00},
+            '1kg': {'preco_venda': 18.00, 'preco_custo': 16.20},
+        },
+        'normal': {
+            '0.5kg': {'preco_venda': 7.00, 'preco_custo': 6.30},
+            '1kg': {'preco_venda': 14.00, 'preco_custo': 12.60},
+        },
+    },
+}
+
 class Usuarios(models.Model):
     ROLE_CHOICES = (
         ('admin', 'Administrador'),
@@ -15,6 +68,9 @@ class Usuarios(models.Model):
 class Produto(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=255)
+    animal = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50)
+    peso = models.DecimalField(max_digits=4, decimal_places=2)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     preco_de_custo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     quantidade = models.PositiveIntegerField(default=0)
